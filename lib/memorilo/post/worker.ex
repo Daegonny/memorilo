4b0432f26c41fa49bfee6524f579b5eea74136a4
Daegonny <igorchagasm@hotmail.com>
2022-09-12 18:55:43 -0300

    refact: use new time utils and logger

--- lib/memorilo/post/worker.ex
+++ lib/memorilo/post/worker.ex
@@ -2,9 +2,11 @@ defmodule Memorilo.Post.Worker do
   @moduledoc """
   Schedule message and publish then at deadline
   """
+  require Logger
   use GenServer, restart: :transient
-  alias Memorilo.Post.{Utils, Message, Delivery}
+  alias Memorilo.Post.Delivery
   alias Memorilo.Mail.Mailer
+  alias Memorilo.TimeUtils
 
   def start_link(%Delivery{} = delivery) do
     GenServer.start_link(__MODULE__, delivery)
@@ -17,17 +19,17 @@ defmodule Memorilo.Post.Worker do
   end
 
   @impl true
-  def handle_info(:schedule, %Delivery{message: message, delivery_time: delivery_time} = delivery) do
-    wating_time_in_ms = Utils.compute_waiting_time_in_ms(delivery_time)
-    :timer.send_after(wating_time_in_ms, {:deliver, message})
-    IO.puts("#{inspect(self())} scheduled a message to #{delivery_time}")
+  def handle_info(:schedule, %Delivery{delivery_time: delivery_time} = delivery) do
+    wating_time_in_ms = TimeUtils.compute_waiting_time_in_ms(delivery_time)
+    :timer.send_after(wating_time_in_ms, {:deliver, delivery})
+    Logger.info("#{inspect(self())} scheduled a message to #{delivery_time}")
     {:noreply, delivery}
   end
 
-  def handle_info({:deliver, %Message{to: to, content: content} = message}, _state) do
-    mail_message = Mailer.build_message(to, "Memori", content)
-    Mailer.send(mail_message)
-    IO.puts("#{inspect(self())} sends #{inspect(message)}")
+  def handle_info({:deliver, %Delivery{subject: subject, content: content, to: to} = delivery}, _state) do
+    message = Mailer.build_message(subject, content, to)
+    Mailer.send(message)
+    Logger.info("#{inspect(self())} sends #{inspect(delivery)}")
     {:stop, :normal, :ok}
   end
 end
